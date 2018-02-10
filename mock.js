295af70fe08d3066dfc80ec1e7ac386d28a56d95
cindy2011 <weimengmeng2011@163.com>
2018-02-10 10:56:39 +0800

    v1.1

--- mock.js
+++ mock.js
@@ -1,7 +1,7 @@
 var Mock = require('mockjs')
 exports.data = function() {
     return [{
-        route: '/sidemenu',//这里的路径需要跟页面请求的路径一样
+        route: '/sidemenu', //这里的路径需要跟页面请求的路径一样
         handle: function(req, res, next) {
             //req   请求头
             //res   响应的数据
@@ -22,6 +22,21 @@ exports.data = function() {
                         'id': "@integer(0,10)"
                     }]
                 }]
+            });
+            res.write(JSON.stringify(data));
+            res.end(); //有开头有结尾不然数据依然无返回
+        }
+    }, {
+        route: '/num', //这里的路径需要跟页面请求的路径一样
+        handle: function(req, res, next) {
+            //req   请求头
+            //res   响应的数据
+            // res请求头是模拟的后台数据返回告诉浏览器返回数据头，没有头的话数据出不来的
+            res.writeHead(200, {
+                "Content-type": "application/json;charset=UTF-8"
+            });
+            var data = Mock.mock({
+                "number|1-100": 100
             });
             res.write(JSON.stringify(data));
             res.end(); //有开头有结尾不然数据依然无返回
